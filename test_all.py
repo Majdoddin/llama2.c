5f066de157c4d982bfdf5b91fa67839cd81c1975
Ruhollah Majdoddin <r.majdodin@gmail.com>
2023-08-15 10:22:42 +0000

    bla

--- test_all.py
+++ test_all.py
@@ -47,12 +47,17 @@ def test_runc():
     model_path = os.path.join(test_ckpt_dir, "stories260K.bin")
     tokenizer_path = os.path.join(test_ckpt_dir, "tok512.bin")
     command = ["./run", model_path, "-z", tokenizer_path, "-t", "0.0", "-n", "200"]
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=False)
+    with open('output.txt', 'wb') as file:
+        # Create a Popen object with the program and arguments
+        proc = subprocess.Popen(command, stdout=file, stderr=subprocess.PIPE, text=False)
 
-    stdout, stderr = proc.communicate()
-    stdout = stdout.decode('utf-8').encode('ascii')
+    #stdout, stderr = proc.communicate()
+    #stdout = stdout.decode('utf-8').encode('ascii')
     # strip the very last \n that is added by run.c for aesthetic reasons
-    stdout = stdout[:-1]
+    with open('output.txt', 'r', encoding='utf-8') as file:
+        # Read the content of the file
+        stdout = file.read().encode('ascii')
+        #stdout = stdout[:-1]
     assert stdout == expected_stdout
 
 def test_python():
